dbeb65fffe70a1eddda7e5c2decbf22a94a41f70
Omar <mourra24@gmail.com>
2023-01-17 00:08:51 +0200

    first iteration
    added simple code for the camera and the scene
    added floor and simple colored materials
    doing the desk next then the pc after

---
+++ README.md
@@ -0,0 +1,30 @@
+# My desk Threejs
+
+A simple Threejs scene where I modeled a simple disk using a blender.
+
+## How to Run
+
+to make the project work you follow these steps.
+to initialize the npm environment
+
+```ssh
+npm init -y
+```
+
+to install parcel files, project manager
+
+```ssh
+npm install parcel
+```
+
+to install Threejs the 3D game/web engine
+
+```ssh
+npm install three
+```
+
+then you can deploy the web page using parcel by executing the following command in the terminal
+
+```ssh
+parcel ./src/index.html
+```
---
+++ src/js/script.js
@@ -0,0 +1,104 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+
+
+const scenemodel = new URL('../models/scene.gltf', import.meta.url);
+//const floortext = new URL('../models/floor.png', import.meta.url);
+
+//init renderer/scene /camera/lights
+//3d model loader
+const loader = new GLTFLoader();
+const scene = new THREE.Scene();
+//camera
+//##########################################
+const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 500);//camera config
+camera.position.set(0, 0, 15);//camera position
+camera.lookAt(0, 0, 0);
+
+//camera rotation
+//###########################################
+//lights
+//const light = new THREE.PointLight(0xFFE0BD, 2, 1000);
+const sun = new THREE.HemisphereLight(0xffffff, 0x000000, 4);
+//renderer and linking it to the html
+const canvas = document.querySelector('#c');
+const renderer = new THREE.WebGLRenderer({ canvas });
+renderer.setClearColor(0xEEEEEE); //background color
+
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.update();
+controls.enableDamping = true
+// controls.enableRotate = true
+renderer.outputEncoding = THREE.sRGBEncoding;   
+//scene.add(light);
+scene.add(sun);
+// const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+// const material = new THREE.MeshPhongMaterial( { color: 0x00ff00 } );
+// const cube = new THREE.Mesh( geometry, material );
+// scene.add( cube );
+
+camera.position.z = 5;
+//this function is called to render every frame during runtime and to adjust screensize in case something changed
+function render() {
+    //window resizing
+    if (resizeRendererToDisplaySize(renderer)) {
+        const canvas = renderer.domElement;
+        camera.aspect = canvas.clientWidth / canvas.clientHeight;
+        camera.updateProjectionMatrix();
+    }
+
+    renderer.render(scene, camera);
+    controls.update();
+    requestAnimationFrame(render);
+}
+
+let floor;
+
+
+
+loader.load(
+    // resource URL
+    scenemodel.href,
+    // called when the resource is loaded
+    function (gltf) {
+        scene.add(gltf.scene);
+        gltf.animations; // Array<THREE.AnimationClip>
+        gltf.scene; // THREE.Group
+        gltf.scenes; // Array<THREE.Group>
+        gltf.cameras; // Array<THREE.Camera>
+        gltf.asset; // Object
+    },
+    // called while loading is progressing
+    function (xhr) {
+
+        console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+        
+    },
+    // called when loading has errors
+    function (error) {
+
+        console.log('An error happened');
+
+    }
+);
+
+
+
+
+
+//function to check if we need to resize the screen pixels height and width
+function resizeRendererToDisplaySize(renderer) {
+    const canvas = renderer.domElement;
+    const width = canvas.clientWidth;
+    const height = canvas.clientHeight;
+    const needResize = canvas.width !== width || canvas.height !== height;
+    if (needResize) {
+        renderer.setSize(width, height, false);
+    }
+    return needResize;
+}
+
+
+//to start requestion animation frame loop like while(1)
+requestAnimationFrame(render);
