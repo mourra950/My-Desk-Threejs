f92948496223736149832f133d23c05af5f85cfd
Omar <mourra24@gmail.com>
2023-01-17 23:08:22 +0200

    commit

--- src/js/script.js
+++ src/js/script.js
@@ -4,51 +4,36 @@ import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
 
 
-const scenemodel = new URL('../models/SQUIDHOUSE.gltf', import.meta.url);
+
+
+const scenemodel = new URL('../models/scene.gltf', import.meta.url);
 //init renderer/scene /camera/lights
 //3d model loader
 const loader = new GLTFLoader();
 const scene = new THREE.Scene();
-
 //camera
 //##########################################
-const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);//camera config
-camera.position.set(0, 300, 650);//camera position
+const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.set(0, 20, 0);//camera position
 camera.lookAt(0, 0, 0);
 
 //camera rotation
 //###########################################
 //lights
 //const light = new THREE.PointLight(0xFFE0BD, 2, 1000);
-const sun = new THREE.HemisphereLight(0xffffff, 0x000000, 2);
-
+const sun = new THREE.HemisphereLight(0xffffff, 0x000000, 1);
+scene.add(sun);
 
 //
 const canvas = document.querySelector('#c');
 const renderer = new THREE.WebGLRenderer( { canvas } );
-renderer.alpha=true
-
-
-renderer.setClearColor( 0xEEEEEE); // the default
-renderer.setPixelRatio( window.devicePixelRatio );
-renderer.setSize( window.innerWidth, window.innerHeight );
-//document.body.appendChild( renderer.domElement );
-
-
+renderer.setClearColor(new THREE.Color(0x0C2D48));
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.update();
 controls.enableDamping = true
-// controls.enableRotate = true
 renderer.outputEncoding = THREE.sRGBEncoding;   
-//scene.add(light);
-scene.add(sun);
-// const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-// const material = new THREE.MeshPhongMaterial( { color: 0x00ff00 } );
-// const cube = new THREE.Mesh( geometry, material );
-// scene.add( cube );
 
-camera.position.z = 5;
-//this function is called to render every frame during runtime and to adjust screensize in case something changed
+
 function render() {
     //window resizing
     if (resizeRendererToDisplaySize(renderer)) {
@@ -56,16 +41,11 @@ function render() {
         camera.aspect = canvas.clientWidth / canvas.clientHeight;
         camera.updateProjectionMatrix();
     }
-        
+     
     renderer.render(scene, camera);
-
-    
-    
-    controls.update();
     requestAnimationFrame(render);
 }
 
-let floor;
 
 
 
@@ -75,11 +55,33 @@ loader.load(
     // called when the resource is loaded
     function (gltf) {
         scene.add(gltf.scene);
+        
         gltf.animations; // Array<THREE.AnimationClip>
         //gltf.scene.scale.set(10,10,10); // THREE.Group
         gltf.scenes; // Array<THREE.Group>
         gltf.cameras; // Array<THREE.Camera>
         gltf.asset; // Object
+        // html2canvas(htmlContainer).then(canvas1=> {
+        //     console.log(gltf.scene.getObjectByName('screen').material)
+        //     // Create a Three.js texture from the canvas
+        //     const texture = new THREE.CanvasTexture(canvas1);
+        //     const material = new THREE.MeshBasicMaterial({map: texture});
+        //     gltf.scene.getObjectByName('screen').material=material
+        //    
+        console.log(gltf.scene.getObjectByName('screen').material)
+        //   });
+
+        // html2canvas(htmlContainer).then(canvas => {
+        //     // Create a Three.js texture from the canvas
+        //     const texture = new THREE.CanvasTexture(canvas);
+          
+            
+        //     const material = new THREE.MeshBasicMaterial({map: texture});
+        //     gltf.scene.getObjectByName('screen').material=material
+        //     // Render the scene
+        //     //renderer.render(scene, camera);
+        //   });
+        
     },
     // called while loading is progressing
     function (xhr) {
@@ -111,7 +113,23 @@ function resizeRendererToDisplaySize(renderer) {
     return needResize;
     
 }
-
+const button = document.getElementById('my-button');
+button.addEventListener('click', () => {
+  alert('Button clicked!');
+});
 
 //to start requestion animation frame loop like while(1)
-requestAnimationFrame(render);
+
+
+
+
+
+
+
+
+    
+
+   
+    
+    
+    requestAnimationFrame(render);
